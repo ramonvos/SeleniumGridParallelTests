710ab8f749bcd564879e0187f614750989a8e0d4
Ramon Vinicius <ramon.souza1988@gmail.com>
2018-07-28 12:45:05 -0300

    Initial commit

---
+++ SeleniumGridTest/Tests/GoogleFindTest.cs
@@ -0,0 +1,132 @@
+﻿using NUnit.Framework;
+using SeleniumGridTest.AdvancedReport;
+using SeleniumGridTest.Base;
+using SeleniumGridTest.Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumGridTest.Tests
+{
+    [TestFixture]
+    public class GoogleFindTest : BaseTest
+    {   
+
+        [Test,Description("Desc"), Category("Critical")]
+        public void FindGooglePageSuccess1()
+        {   
+            AdvancedReport.Reporter.AddTest();
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+
+            Reporter.TestInfo();
+            Reporter.TestInfo();
+            Reporter.TestInfo();
+            Reporter.TestInfo();
+            Reporter.TestInfo();
+
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selenium Tutorials - Guru99"), Is.EqualTo(true),"Erro ao validar o texto!");
+            Reporter.TestPass();
+
+        }
+
+        [Test, Description("Desc"), Category("Smoke test")]
+        public void FindGooglePageFail1()
+        {
+            
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+            Reporter.TestInfo();
+
+            Reporter.TestFail();
+
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selium Tutorials - Guru99"), Is.EqualTo(true), "Erro ao validar o label");
+           
+            
+            
+
+        }
+
+
+        [Test, Description("Desc"), Category("Exploratory")]
+        public void FindGooglePageSuccess2()
+        {
+            
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+
+            Reporter.TestPass();
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selenium Tutorials - Guru99"), Is.EqualTo(true), "Erro ao validar o texto!");
+            
+
+        }
+        [Test, Description("Desc"), Category("Exploratory")]
+        public void FindGooglePageSuccess3()
+        {
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+            Reporter.TestPass();
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selenium Tutorials - Guru99"), Is.EqualTo(true), "Erro ao validar o texto!");
+            
+
+        }
+
+        [Test, Description("Desc"), Category("Exploratory")]
+        public void FindGooglePageSuccess4()
+        {
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+            Reporter.TestPass();
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selenium Tutorials - Guru99"), Is.EqualTo(true), "Erro ao validar o texto!");
+            
+
+        }
+
+        [Test, Description("Desc"), Category("Exploratory")]
+        public void FindGooglePageSuccess5()
+        {
+            
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+            Reporter.TestPass();
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selenium Tutorials - Guru99"), Is.EqualTo(true), "Erro ao validar o texto!");
+            
+
+        }
+
+        [Test, Description("Desc"), Category("Exploratory")]
+        public void FindGooglePageSuccess6()
+        {
+
+            string text = "Selenium lesson page";
+            GooglePage page = new GooglePage();
+
+            page.OpenGooglePage().FindGoogle(text);
+            Reporter.TestPass();
+            Assert.That(BaseTest.driver.PageSource.Contains("Free Selenium Tutorials - Guru99"), Is.EqualTo(true), "Erro ao validar o texto!");
+            
+
+        }
+    }
+}
