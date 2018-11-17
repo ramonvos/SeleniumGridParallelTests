e867aede690dfba899926dc91cdf6cb5dcf0bd86
Ramon Vinicius <ramon.souza1988@gmail.com>
2018-11-17 14:40:59 -0200

    Remove pasta bin e obj

--- SeleniumGridTest.Helpers/bin/Debug/SeleniumRun.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\ramon\source\repos\SeleniumGridTest\packages\NUnit.ConsoleRunner.3.8.0\tools
-nunit3-console.exe C:\Users\ramon\source\repos\SeleniumGridTest\SeleniumGridTest\bin\Debug\SeleniumGridTest.dll
